371fbb63a2aea605bcd88c4e057c93d7b6c2fa21
robmckinnon <rob@movingflow>
2023-05-03 22:46:57 +0100

    Add RefactorExtractFunction to handler code_actions

--- apps/language_server/lib/language_server/experimental/provider/handlers/code_action.ex
+++ apps/language_server/lib/language_server/experimental/provider/handlers/code_action.ex
@@ -1,4 +1,5 @@
 defmodule ElixirLS.LanguageServer.Experimental.Provider.Handlers.CodeAction do
+  alias ElixirLS.LanguageServer.Experimental.Provider.CodeAction.RefactorExtractFunction
   alias ElixirLS.LanguageServer.Experimental.Provider.CodeAction.ReplaceRemoteFunction
   alias ElixirLS.LanguageServer.Experimental.Provider.CodeAction.ReplaceWithUnderscore
   alias ElixirLS.LanguageServer.Experimental.Provider.Env
@@ -7,7 +8,11 @@ defmodule ElixirLS.LanguageServer.Experimental.Provider.Handlers.CodeAction do
 
   require Logger
 
-  @code_actions [ReplaceRemoteFunction, ReplaceWithUnderscore]
+  @code_actions [
+    RefactorExtractFunction,
+    ReplaceRemoteFunction,
+    ReplaceWithUnderscore
+  ]
 
   def handle(%Requests.CodeAction{} = request, %Env{}) do
     code_actions =
